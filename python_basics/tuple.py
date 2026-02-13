f0f8096b6465eec3012a6ded6d17157bafec79b6
lokanath99 <lokanath1912@gmil.com>
2026-02-13 11:24:15 +0530

    13/2

--- python_basics/tuple.py
+++ python_basics/tuple.py
@@ -1,9 +1,24 @@
 tp = (6,9,8,6)
+a = "loka"
 # tp[0] = 85
 print(tp)
 lst = [569,98,7,8,9,8,69,68]
 lst.append('asdfasdf')
 print(lst)
 
-dt = {"key":"val"}
-print(dt["key"])
+# car = {
+#     "brand": "VW",
+#     "model": "Polo",
+#     "engine": {"base":"1.5 TDI", "Top":"1.6 TDI"}
+# }
+# print(car["engine"]["base"])
+# print(car.items())
+
+# st = {1,2,3,2,4,5}
+# st1 = {3,4,5,6}
+# print(st)
+# print(st.intersection(st1))
+
+# a = int(92423424.423423424)
+
+# print(list(range(0,9,2)))
